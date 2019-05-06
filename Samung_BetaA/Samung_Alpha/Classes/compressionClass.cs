36f28a8e4409cbd01253bc4ff4e825f3c64eb8b0
YairE <yair200111@gmail.com>
2019-05-06 12:28:58 +0300

    yy

---
+++ Samung_BetaA/Samung_Alpha/Classes/compressionClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samung_Alpha.Classes
+{
+    class compressionClass
+    {
+        private static void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+
+        public static byte[] Zip(string str)
+        { //Compresses string and encrypts to base64
+
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public static string Unzip(byte[] bytes)
+        { //Decrypts from base64 and decompresses the string
+
+            //byte[] bytes = Convert.FromBase64String(str);
+
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
+                    CopyTo(gs, mso);
+                }
+
+                return Encoding.UTF8.GetString(mso.ToArray());
+            }
+        }
+    }
+}
---
+++ Samung_BetaA/Samung_Alpha/Form8.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Samung_Alpha
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatBox = new System.Windows.Forms.RichTextBox();
+            this.writeBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // chatBox
+            // 
+            this.chatBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.chatBox.Location = new System.Drawing.Point(12, 37);
+            this.chatBox.Name = "chatBox";
+            this.chatBox.ReadOnly = true;
+            this.chatBox.Size = new System.Drawing.Size(494, 362);
+            this.chatBox.TabIndex = 0;
+            this.chatBox.Text = "";
+            // 
+            // writeBox
+            // 
+            this.writeBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.writeBox.Location = new System.Drawing.Point(12, 405);
+            this.writeBox.Name = "writeBox";
+            this.writeBox.Size = new System.Drawing.Size(494, 46);
+            this.writeBox.TabIndex = 1;
+            this.writeBox.Text = "";
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(50)))), ((int)(((byte)(67)))));
+            this.ClientSize = new System.Drawing.Size(518, 463);
+            this.Controls.Add(this.writeBox);
+            this.Controls.Add(this.chatBox);
+            this.Name = "Form8";
+            this.Text = "Form8";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form8_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox chatBox;
+        private System.Windows.Forms.RichTextBox writeBox;
+    }
+}
